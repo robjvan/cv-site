100e8a9c8b2601fe6fd2d97eab2a7faf4b84c52f
Rob Vandelinder <robjvan@gmail.com>
2025-04-14 17:19:10 -0230

    Added placeholder files

---
+++ src/app/services/location.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocationService {
+  constructor() {}
+
+  getUserLocation(ip: string) {
+    // TODO(RV): Add logic to get user location by IP
+    // Use location to show local weather data
+  }
+}
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  constructor() {}
+
+  fetchWeatherData() {
+    // TODO(RV): Add logic to fetch weather data
+  }
+}
