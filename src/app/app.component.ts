7215a3c63473d5056c1b082aa9afed415bb185fd
Rob Vandelinder <robjvan@gmail.com>
2025-04-14 16:38:43 -0230

    Replaced boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CupertinoBarComponent } from './components/cupertino-bar/cupertino-bar.component';
+import { AppDockComponent } from './components/app-dock/app-dock.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CupertinoBarComponent, AppDockComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'cv-site';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  title = 'Welcome to my site';
+
+  // useDarkMode: WritableSignal<boolean> = signal<boolean>(true);
+
+  ngOnInit(): void {
+    // this.themeService.useDarkMode$.subscribe({
+    //   next: (val) => {
+    //     this.useDarkMode.set(val);
+    //   },
+    //   error: (err) => console.log(err),
+    // });
+  }
 }
