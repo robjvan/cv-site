2cbccbede05848eed2d86394ba7b5ece4caf1e82
Rob Vandelinder <robjvan@gmail.com>
2025-04-14 17:38:07 -0230

    Combined statusMap and statusColorMap

--- src/app/components/dialogs/todos-dialog/todo-card/todo-card.component.ts
+++ src/app/components/dialogs/todos-dialog/todo-card/todo-card.component.ts
@@ -4,7 +4,6 @@ import { TodoService } from '../../../../services/todo.service';
 import { TodoStatus } from '../../../../models/enums/todo-status.enum';
 import moment from 'moment';
 import { CommonModule } from '@angular/common';
-import { DialogPurpose } from '../../../../models/enums/dialog-purpose.enum';
 
 @Component({
   selector: 'todo-card',
@@ -22,38 +21,33 @@ export class TodoCardComponent {
     this.todosService.deleteTodo(this.todo()!.id);
   }
 
-  private readonly statusMap: Record<TodoStatus, string> = {
-    [TodoStatus.NEW]: 'NEW',
-    [TodoStatus.ON_HOLD]: 'ON HOLD',
-    [TodoStatus.CANCELLED]: 'CANCELLED',
-    [TodoStatus.COMPLETE]: 'COMPLETE',
-    [TodoStatus.IN_PROGRESS]: 'IN PROGRESS',
+  private readonly statusMap: Record<
+    TodoStatus,
+    { status: string; colorClass: string }
+  > = {
+    [TodoStatus.NEW]: { status: 'NEW', colorClass: 'new' },
+    [TodoStatus.ON_HOLD]: { status: 'ON HOLD', colorClass: 'onhold' },
+    [TodoStatus.CANCELLED]: { status: 'CANCELLED', colorClass: 'cancelled' },
+    [TodoStatus.COMPLETE]: { status: 'COMPLETE', colorClass: 'complete' },
+    [TodoStatus.IN_PROGRESS]: {
+      status: 'IN PROGRESS',
+      colorClass: 'inprogress',
+    },
   };
 
-  private readonly statusClassMap: Record<TodoStatus, string> = {
-    [TodoStatus.NEW]: 'new',
-    [TodoStatus.ON_HOLD]: 'onhold',
-    [TodoStatus.CANCELLED]: 'cancelled',
-    [TodoStatus.COMPLETE]: 'complete',
-    [TodoStatus.IN_PROGRESS]: 'inprogress',
-  };
-
-  get status() {
-    const status = this.todo()!.status;
-    return this.statusMap[status];
+  get status(): string {
+    return this.statusMap[this.todo()!.status].status;
   }
 
   get dueDate(): string | null {
     if (this.todo()?.dueDate) {
-      // return moment(this.todo()?.dueDate).format('ddd MMM Do h:mm A');
       return moment(this.todo()?.dueDate).format('dddd MMMM Do');
+    } else {
+      return null;
     }
-
-    return null;
   }
 
-  getColorClass() {
-    const status = this.todo()!.status;
-    return this.statusClassMap[status];
+  getColorClass(): string {
+    return this.statusMap[this.todo()!.status].colorClass;
   }
 }
