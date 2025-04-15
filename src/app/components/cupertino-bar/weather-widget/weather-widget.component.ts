d9abe8d2ce68cd4d5bcd662a0008663621862584
Rob Vandelinder <robjvan@gmail.com>
2025-04-14 21:35:49 -0230

    Added data fetching

--- src/app/components/cupertino-bar/weather-widget/weather-widget.component.ts
+++ src/app/components/cupertino-bar/weather-widget/weather-widget.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { WeatherService } from '../../../services/weather.service';
+import { IForecastData } from '../../../models/forecast-data.interface';
+import { DecimalPipe } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
+import { LocationService } from '../../../services/location.service';
 
 @Component({
   selector: 'weather-widget',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './weather-widget.component.html',
   styleUrl: './weather-widget.component.scss',
 })
-export class WeatherWidgetComponent {}
+export class WeatherWidgetComponent implements OnInit {
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private readonly weatherService: WeatherService,
+    private readonly locationService: LocationService
+  ) {}
+
+  public forecastData = signal<IForecastData | undefined>(undefined);
+  public locationAllowed = signal<boolean>(false);
+
+  ngOnInit(): void {
+    this.locationService.locationAllowed$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (locationAllowed: boolean) =>
+          this.locationAllowed.set(locationAllowed),
+        error: (err) => console.error(err),
+      });
+
+    this.weatherService.forecastData$.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (forecastData: IForecastData | undefined) =>
+        this.forecastData.set(forecastData),
+      error: (err) => console.error(err),
+    });
+  }
+}
--- src/app/components/dialogs/weather-dialog/weather-dialog.component.ts
+++ src/app/components/dialogs/weather-dialog/weather-dialog.component.ts
@@ -1,25 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { DialogWindowComponent } from '../../dialog-window/dialog-window.component';
 import { DialogPurpose } from '../../../models/enums/dialog-purpose.enum';
 import { LauncherService } from '../../../services/launcher.service';
-import { Subject, takeUntil } from 'rxjs';
+import { filter, Subject, takeUntil } from 'rxjs';
+import { LocationService } from '../../../services/location.service';
+import { WeatherService } from '../../../services/weather.service';
+import { IForecastData } from '../../../models/forecast-data.interface';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'weather-dialog',
-  imports: [DialogWindowComponent],
+  imports: [DialogWindowComponent, DecimalPipe],
   templateUrl: './weather-dialog.component.html',
-  styleUrl: './weather-dialog.component.scss',
+  styleUrls: ['./weather-dialog.component.scss'],
 })
-export class WeatherDialogComponent {
+export class WeatherDialogComponent implements OnInit {
   private destroy$ = new Subject<void>();
-  constructor(private readonly launcherService: LauncherService) {}
 
-  ngOnDestroy(): void {
+  // Signals
+  public forecastData = signal<IForecastData | undefined>(undefined);
+  private hasRequestedWeather = false;
+  public locationAllowed = signal<boolean>(false);
+
+  // Reactive computed signal
+  public isLoading = computed(() => {
+    return !this.forecastData() && this.hasRequestedWeather;
+  });
+
+  constructor(
+    private readonly launcherService: LauncherService,
+    private readonly locationService: LocationService,
+    private readonly weatherService: WeatherService
+  ) {}
+
+  public ngOnInit() {
+    this.locationService.locationAllowed$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (locationAllowed: boolean) =>
+          this.locationAllowed.set(locationAllowed),
+        error: (err) => console.error(err),
+      });
+
+    // Handle weather data
+    this.weatherService.forecastData$.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (data) => {
+        this.forecastData.set(data);
+      },
+      error: (err) =>
+        console.error('[WeatherDialogComponent] Forecast error:', err.message),
+    });
+  }
+
+  public ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
-  closeDialog() {
+  public refreshWeather(): void {
+    this.forecastData.set(undefined);
+    this.weatherService.fetchWeatherData();
+  }
+
+  public closeDialog() {
     this.launcherService.closeDialog(DialogPurpose.WEATHER);
   }
 }
