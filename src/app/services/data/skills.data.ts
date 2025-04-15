34181b7b47de7d9cde2e7660203e695cca6a0a63
Rob Vandelinder <robjvan@gmail.com>
2025-04-14 22:39:59 -0230

    Added skills data

---
+++ src/app/services/data/skills.data.ts
@@ -0,0 +1,120 @@
+import { ISkillMetric } from '../../models/skill-metric.interface';
+
+export const skillsData: ISkillMetric[] = [
+  {
+    title: 'Back end development',
+    value: 8,
+  },
+  {
+    title: 'Front end development',
+    value: 7,
+  },
+  {
+    title: 'Databases (SQL/NoSQL)',
+    value: 4,
+  },
+  {
+    title: 'Hardware Repair',
+    value: 8,
+  },
+  {
+    title: 'Server Administration',
+    value: 5,
+  },
+  {
+    title: 'Technical Support/Repair',
+    value: 9,
+  },
+  {
+    title: 'Technical Writing',
+    value: 6,
+  },
+  {
+    title: '3D Modeling',
+    value: 6,
+  },
+];
+
+export const frameworks: ISkillMetric[] = [
+  {
+    title: 'Angular 2+',
+    value: 6,
+  },
+  {
+    title: 'Angular Material',
+    value: 4,
+  },
+  {
+    title: 'Bootstrap',
+    value: 7,
+  },
+  {
+    title: 'Flutter',
+    value: 8,
+  },
+  {
+    title: 'NestJS',
+    value: 7,
+  },
+  {
+    title: 'Svelte',
+    value: 2,
+  },
+];
+
+export const languages: ISkillMetric[] = [
+  {
+    title: 'Dart',
+    value: 6,
+  },
+  {
+    title: 'Typescript',
+    value: 7,
+  },
+  {
+    title: 'Python',
+    value: 3,
+  },
+  {
+    title: 'C#',
+    value: 2,
+  },
+  {
+    title: 'SQL',
+    value: 3,
+  },
+];
+
+export const cloudPlatforms: ISkillMetric[] = [
+  {
+    title: 'Google Cloud/Firebase',
+    value: 4,
+  },
+  {
+    title: 'AWS',
+    value: 2,
+  },
+  {
+    title: 'Azure',
+    value: 0,
+  },
+  {
+    title: 'Git',
+    value: 5,
+  },
+];
+
+export const operatingSystems: ISkillMetric[] = [
+  {
+    title: 'Windows',
+    value: 9,
+  },
+  {
+    title: 'Mac OS',
+    value: 7,
+  },
+  {
+    title: 'Linux',
+    value: 5,
+  },
+];
