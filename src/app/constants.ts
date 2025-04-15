dd761cc0ade3f0474b567c24d55feb1921d1d384
Rob Vandelinder <robjvan@gmail.com>
2025-04-14 21:40:06 -0230

    Added constants file

---
+++ src/app/constants.ts
@@ -0,0 +1,4 @@
+export const apiUrl = 'https://api.weatherapi.com/v1/forecast.json?key=';
+
+export const apiKey = '8555ce64325240e9a71211345211206';
+export const googleApiKey = 'AIzaSyA8FyRVHdJDgEjetGNg5XMdLmsvHZi8VkQ';
