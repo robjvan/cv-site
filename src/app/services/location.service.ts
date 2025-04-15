6bd42520c29804c8ac5abb44695ec584606aa9f9
Rob Vandelinder <robjvan@gmail.com>
2025-04-14 21:39:05 -0230

    Added location fetching

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { IUserLocation } from '../models/user-location.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -6,8 +8,44 @@ import { Injectable } from '@angular/core';
 export class LocationService {
   constructor() {}
 
-  getUserLocation(ip: string) {
-    // TODO(RV): Add logic to get user location by IP
-    // Use location to show local weather data
+  public location$ = new BehaviorSubject<IUserLocation | undefined>(undefined);
+  public locationAllowed$ = new BehaviorSubject<boolean>(false);
+
+  public getUserLocation() {
+    console.debug('[LocationService] Determining user location...');
+
+    if (!navigator.geolocation) {
+      console.warn(
+        '[LocationService] Geolocation is not supported by this browser.'
+      );
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          this.setLocationAllowed(true);
+          const coords: IUserLocation = {
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          };
+          this.location$.next(coords);
+          console.debug('[LocationService] Location determined:', coords);
+        },
+        (err: GeolocationPositionError) => {
+          this.setLocationAllowed(false);
+          console.error(
+            '[LocationService] Error getting location:',
+            err.message
+          );
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 10000,
+          maximumAge: 60000,
+        }
+      );
+    }
+  }
+
+  public setLocationAllowed(value: boolean) {
+    this.locationAllowed$.next(value);
   }
 }
