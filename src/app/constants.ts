7ff8ce4443a9f7075adb230210d019e369c72a41
Rob Vandelinder <robjvan@gmail.com>
2025-04-18 10:01:24 -0230

    Added color constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,24 @@
+import * as THREE from 'three';
+
 export const apiUrl = 'https://api.weatherapi.com/v1/forecast.json?key=';
 
 export const apiKey = '8555ce64325240e9a71211345211206';
 export const googleApiKey = 'AIzaSyA8FyRVHdJDgEjetGNg5XMdLmsvHZi8VkQ';
+
+// Colors values, used in the STL viewer dialog
+export const BLACK: string = '0xff131313';
+export const RED: string = '0xffff0000';
+export const GREEN: string = '0xff007700';
+export const BLUE: string = '0xff0000aa';
+export const WHITE: string = '0xffffffff';
+export const PURPLE: string = '0xff8800aa';
+export const PINK: string = '0xffff66aa';
+export const YELLOW: string = '0xffffca28';
+export const ORANGE: string = '0xffda660b';
+export const LIGHTBLUE: string = '0xff47ffff';
+
+export const BG_GREY: THREE.ColorRepresentation = 0xa0a0a0;
+export const BG_MED_GREY: THREE.ColorRepresentation = 0x444444;
+export const BG_DARK_GREY: THREE.ColorRepresentation = 0x101010;
+export const BG_WHITE: THREE.ColorRepresentation = 0xffffff;
+export const BG_BLACK: THREE.ColorRepresentation = 0x000000;
