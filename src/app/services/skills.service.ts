508f9b75a2c112cc148317c0fdfae0afe6117efa
Rob Vandelinder <robjvan@gmail.com>
2025-04-18 16:14:55 -0230

    Added skills data

--- src/app/services/skills.service.ts
+++ src/app/services/skills.service.ts
@@ -6,25 +6,43 @@ import {
   operatingSystems,
   skillsData,
 } from './data/skills.data';
+import { BehaviorSubject } from 'rxjs';
+import { ISkillMetric } from '../models/skill-metric.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SkillsService {
-  public get cloudPlatforms() {
-    return cloudPlatforms;
-  }
+  // cloudPlatforms$ = new BehaviorSubject<ISkillMetric[]>(cloudPlatforms);
+  // frameworks$ = new BehaviorSubject<ISkillMetric[]>(frameworks);
+  // languages$ = new BehaviorSubject<ISkillMetric[]>(languages);
+  // operatingSystems$ = new BehaviorSubject<ISkillMetric[]>(operatingSystems);
+  // skillsData$ = new BehaviorSubject<ISkillMetric[]>(skillsData);
 
-  public get frameworks() {
-    return frameworks;
-  }
-  public get languages() {
-    return languages;
-  }
-  public get operatingSystems() {
-    return operatingSystems;
-  }
-  public get skillsData() {
-    return skillsData;
-  }
+  data$: BehaviorSubject<{
+    cloudPlatforms: ISkillMetric[];
+    skillsData: ISkillMetric[];
+    languages: ISkillMetric[];
+    operatingSystems: ISkillMetric[];
+    frameworks: ISkillMetric[];
+  }> = new BehaviorSubject({
+    cloudPlatforms: cloudPlatforms,
+    skillsData: skillsData,
+    languages: languages,
+    operatingSystems: operatingSystems,
+    frameworks: frameworks,
+  });
+
+  // public get frameworks() {
+  //   return frameworks;
+  // }
+  // public get languages() {
+  //   return languages;
+  // }
+  // public get operatingSystems() {
+  //   return operatingSystems;
+  // }
+  // public get skillsData() {
+  //   return skillsData;
+  // }
 }
