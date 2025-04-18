068da3d616e6cae4e4286c95f7f74375120f683b
Rob Vandelinder <robjvan@gmail.com>
2025-04-18 16:14:21 -0230

    Added color handling

--- src/app/services/stl-viewer.service.ts
+++ src/app/services/stl-viewer.service.ts
@@ -1,8 +1,75 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import {
+  BLACK,
+  RED,
+  GREEN,
+  BLUE,
+  WHITE,
+  PURPLE,
+  PINK,
+  YELLOW,
+  ORANGE,
+  LIGHTBLUE,
+} from '../constants';
+import { IColor } from '../models/color.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StlViewerService {
   constructor() {}
+
+  private colorSet: IColor[] = [
+    {
+      name: 'black',
+      code: BLACK,
+    },
+    {
+      name: 'red',
+      code: RED,
+    },
+    {
+      name: 'green',
+      code: GREEN,
+    },
+    {
+      name: 'blue',
+      code: BLUE,
+    },
+    {
+      name: 'white',
+      code: WHITE,
+    },
+    {
+      name: 'purple',
+      code: PURPLE,
+    },
+    {
+      name: 'pink',
+      code: PINK,
+    },
+    {
+      name: 'yellow',
+      code: YELLOW,
+    },
+    {
+      name: 'orange',
+      code: ORANGE,
+    },
+    {
+      name: 'lightBlue',
+      code: LIGHTBLUE,
+    },
+  ];
+
+  selectedColor$: BehaviorSubject<string> = new BehaviorSubject<string>(ORANGE);
+
+  get colors(): IColor[] {
+    return this.colorSet;
+  }
+
+  setSelectedColor(color: IColor): void {
+    this.selectedColor$.next(`${color.code}`);
+  }
 }
