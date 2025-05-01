1b0bc4a4893f8b12eb406288db44e964a1387f70
Rob Vandelinder <robjvan@gmail.com>
2025-04-30 21:48:48 -0230

    Moved keys

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,10 +1,5 @@
 import * as THREE from 'three';
 
-export const apiUrl = 'https://api.weatherapi.com/v1/forecast.json?key=';
-
-export const apiKey = '8555ce64325240e9a71211345211206';
-export const googleApiKey = 'AIzaSyA8FyRVHdJDgEjetGNg5XMdLmsvHZi8VkQ';
-
 // Colors values, used in the STL viewer dialog
 export const BLACK: string = '0xff131313';
 export const RED: string = '0xffff0000';
